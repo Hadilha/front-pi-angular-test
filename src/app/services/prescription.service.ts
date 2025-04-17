24e48e613c46d0c1611bf94f83e619f2b4e5afc9
Norah <nourelhoudaromdhani@esprit.tn>
2025-04-17 22:59:04 +0100

    Initial commit

--- src/app/services/prescription.service.ts
+++ src/app/services/prescription.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PrescriptionDto } from '../models/prescriptionDto.model';
 
+
 @Injectable({
   providedIn: 'root'
 })
