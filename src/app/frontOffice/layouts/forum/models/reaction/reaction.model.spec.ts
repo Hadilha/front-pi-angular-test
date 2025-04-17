1cc82900567b16b08a4f34e37fa65551b76840a3
Chiha-Mohamed <mohamed.chiha@esprit.tn>
2025-04-17 20:58:58 +0100

    minor fixes v2

--- src/app/frontOffice/layouts/forum/models/reaction/reaction.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Reaction } from './reaction.model';
-
-describe('Reaction', () => {
-  it('should create an instance', () => {
-    expect(new Reaction()).toBeTruthy();
-  });
-});
--- src/app/frontOffice/layouts/forum/pipes/category-filter.pipe.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'categoryFilter' })
-export class CategoryFilterPipe implements PipeTransform {
-  transform(posts: any[], selectedCategory: string): any[] {
-    if (selectedCategory === 'All' || !selectedCategory) return posts;
-    return posts.filter((post) => post.tag === selectedCategory);
-  }
-}
