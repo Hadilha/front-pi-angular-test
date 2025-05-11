da65d17f6ad904b1be1578ff0abbdb9a3aa3b81a
aymenftita <aymen.ftita@esprit.tn>
2025-05-11 18:01:53 +0200

    .
    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  forumImageUploadApi: '1c71f13442msh388738952c1f022p165571jsn107561146aa8',
+  apiUrl: 'http://back:8089/api',
+  aiUrl: 'http://back:8089/ai',
+  wsUrl: 'http://back:8089/ws',
+  url: 'http://back:8089'
 };
